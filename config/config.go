569da65ce0bc10249b9a76766ae40d2aeed936f5
Luis Dourado <lfaddourado@gmail.com>
2022-12-27 19:32:05 -0300

    feat: add struct and vars for the config reading function

--- config/config.go
+++ config/config.go
@@ -1,1 +1,13 @@
 package config
+
+var (
+	Token     string
+	BotPrefix string
+
+	config *configStruct
+)
+
+type configStruct struct {
+	Token  string `json:"token"`
+	Prefix string `json:"prefix"`
+}
