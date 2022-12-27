8242e9081077a2b4676c21758270d862c5f3f8a2
Luis Dourado <lfaddourado@gmail.com>
2022-12-27 19:36:05 -0300

    feat: add reading and unmarshaling the json file functionality

--- config/config.go
+++ config/config.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
 var (
 	Token     string
 	BotPrefix string
@@ -10,4 +16,21 @@ var (
 type configStruct struct {
 	Token  string `json:"token"`
 	Prefix string `json:"prefix"`
+}
+
+func ReadConfig() error {
+	fmt.Println("Lendo o arquivo config.json ...")
+
+	file, err := ioutil.ReadFile("./config.json")
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println(string(file))
+	err = json.Unmarshal(file, config)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
 }
