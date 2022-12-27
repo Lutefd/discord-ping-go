a5a460eeea4df31cf0982170b0bf465960e665cb
Luis Dourado <lfaddourado@gmail.com>
2022-12-27 19:52:54 -0300

    feat: add discordgo import declare variables and import configs

--- bot/bot.go
+++ bot/bot.go
@@ -1,1 +1,21 @@
 package bot
+
+import (
+	"fmt"
+	"github.com/Lutefd/discord-ping-go/config"
+	"github.com/bwmarrin/discordgo"
+)
+
+var BotID string
+var goBot *discordgo.Session
+
+func Start() {
+	fmt.Println("Iniciando o bot ...")
+	discord, err := discordgo.New("Bot " + config.Token)
+
+	if err != nil {
+		fmt.Println("Erro ao iniciar o bot: ", err)
+		return
+	}
+
+}
