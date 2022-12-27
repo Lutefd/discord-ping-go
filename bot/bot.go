0bc6862e6d2cbeab2336491e911fd90b6edef062
Luis Dourado <lfaddourado@gmail.com>
2022-12-27 19:54:17 -0300

    fix: goBot usage

--- bot/bot.go
+++ bot/bot.go
@@ -11,11 +11,10 @@ var goBot *discordgo.Session
 
 func Start() {
 	fmt.Println("Iniciando o bot ...")
-	discord, err := discordgo.New("Bot " + config.Token)
+	goBot, err := discordgo.New("Bot " + config.Token)
 
 	if err != nil {
-		fmt.Println("Erro ao iniciar o bot: ", err)
+		fmt.Println("Erro ao iniciar o bot: ", err.Error())
 		return
 	}
-
 }
